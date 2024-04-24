9957a554fe720aaad728c48e2a9a84506979afde
Luiz André <luizandrenf@gmail.com>
2024-04-24 15:13:27 -0400

    exception handling - update

--- src/main/java/com/luizandrenf/course/services/UserService.java
+++ src/main/java/com/luizandrenf/course/services/UserService.java
@@ -1,7 +1,6 @@
 package com.luizandrenf.course.services;
 
 import java.util.List;
-
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +12,8 @@ import com.luizandrenf.course.entities.User;
 import com.luizandrenf.course.repositories.UserRepository;
 import com.luizandrenf.course.services.exceptions.DatabaseException;
 import com.luizandrenf.course.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class UserService {
@@ -44,9 +45,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);			
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
